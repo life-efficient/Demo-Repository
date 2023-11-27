e1bceb428726f6589c17b92262e5f2aab836cfd6
life-efficient <harryaberg@gmail.com>
2023-11-27 03:39:16 +0000

    Adds all important files

---
+++ imports.py
@@ -0,0 +1,14 @@
+
+# %%
+from mymodule.mycode import x
+
+print(x)
+
+
+#%%
+myarray = a([1, 2, 3])
+another_array = a([4, 5, 6])
+
+#%%
+print(myarray + another_array)
+# %%
---
+++ loops.py
@@ -0,0 +1,14 @@
+# %%
+# loops
+
+shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
+
+for item in shopping_list:
+
+    if item == "spam":
+        continue
+    
+    print(item)
+
+print("done")
+# %%
